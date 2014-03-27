7020e69bb8895154a6c3f71ed1adddffc592f55a
John Schindler <John.Schindler@utexas.edu>
2014-03-27 17:54:41 -0500

    incremental fix, 1st commit was working copy in poor shape

--- toot/toot/spiders/__init__.py
+++ toot/toot/spiders/__init__.py
@@ -17,34 +17,13 @@ class MySpider(Spider):
 	allowed_domains = ["wikipedia.org"]
 	start_urls = ["http://en.wikipedia.org/wiki/2013_Denver_Broncos_season"] 
 
-
-
 	def parse(self, response):
 
 		sel = Selector(response)
 		players = []
 		table =  sel.xpath('//table[@class="toccolours"]')
 		count = 0
-		#for table in sel.xpath("//table[@class="tocolours"]"):
 		for table_row in table.xpath('tr') :
-			print count
-			"""for position in table_row.xpath('b/text()'):
-				player_position = position.extract()"""
-			player_position = table_row.xpath('b/text()').extract()
-			for row in table_row.xpath('ul'):
-				for li in row.xpath('li'):
-					num = li.xpath('span/text()').extract()
-					name = li.xpath('a/text()').extract()
-					player = PlayerItem()
-					player['name'] = name[0]
-					print player["name"]
-					player['position'] = player_position
-					print player["position"]
-					if len(num) > 0:
-						player['number'] = num[0]
-							#print player["number"]
-					players.append(player)
-		"""for table_row in table.xpath('.//tr') :
 			print count
 			for position in table_row.xpath('.//b/text()'):
 				player_position = position.extract()
@@ -63,7 +42,7 @@ class MySpider(Spider):
 						players.append(player)
 
 						#print player['name']
-				count += 1"""
+				count += 1
 		return players
 
 
